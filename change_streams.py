db8268e6384d85f7cacdfa2024a17bc7fefff495
canercak <canercak@gmail.com>
2020-12-09 07:22:14 +0200

    update stream

--- change_streams.py
+++ change_streams.py
@@ -4,7 +4,13 @@ from bson.json_util import dumps
 import sys
 sys.path.insert(0, 'fortress-security-audit-engine')
 import fortress-audit
+from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))w3 = Web3(Web3.IPCProvider('./path/to/geth.ipc'))
+import ConfigParser
 
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+WEB3_INFURA_PROJECT_ID = config.get("myvars", "WEB3_INFURA_PROJECT_ID")
 
 client = pymongo.MongoClient("localhost", 27017)
 change_stream = client.changestream.collection.watch()
@@ -19,6 +25,9 @@ def analyse_for_audit(change):
   for i in change['contracts']:
     audit_results = os.system('python file.py', change['contracts'][i])
     if not audit_results['passed']:
+      if w3.isConnected():
+        w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/'+WEB3_INFURA_PROJECT_ID))
+
 
 
 
